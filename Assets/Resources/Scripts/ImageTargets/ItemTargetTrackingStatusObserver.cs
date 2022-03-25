5be8768a301cc74fd3e010e630060c614da21df8
Daniel Musselwhite <daniel.l.musselwhite@gmail.com>
2022-03-25 16:54:33 +0000

    Modified ImageTracker Observer to be able to be used for multiple different types of ImageTracker objects

--- Assets/Resources/Scripts/ImageTargets/ItemTargetTrackingStatusObserver.cs
+++ Assets/Resources/Scripts/ImageTargets/ItemTargetTrackingStatusObserver.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-
-public class StoatTrackingStatusObserver : MonoBehaviour
+public class ItemTargetTrackingStatusObserver : MonoBehaviour
 {
     private ObserverBehaviour mObserverBehaviour;
     private Status previousState;
-    private static int id;
+    public string baseName;
+    private static Dictionary<string, int> imageTarget_counts = new Dictionary<string, int>();
 
     // Start is called before the first frame update
     void Start()
     {
-        id += 1;
 
         previousState = Status.NO_POSE; // defaulting to not being observed
 
@@ -24,19 +23,20 @@ public class StoatTrackingStatusObserver : MonoBehaviour
     }
     void OnStatusChanged(ObserverBehaviour behaviour, TargetStatus status)
     {
-        //Debug.Log(gameObject.name + " | Status is: "+ status.Status +", statusInfo is: {1} " + status.StatusInfo);
-
         // if the state was not tracked, but it now is ...
-        if (status.Status == Status.TRACKED && previousState != Status.TRACKED) { 
+        if (status.Status == Status.TRACKED && previousState != Status.TRACKED)
+        {
             // ... so lets create another instance of this ImageTarget, in case an identical card is played again
             Debug.Log(gameObject.name + " | Was not tracked before, but now it is");
             // creating a new instance of the ImageTarget for the Stoat, in case an additional Stoat card is played
             var track = Instantiate(gameObject, new Vector3(0, 0, 0), Quaternion.identity);
-            track.name = "ImageTarget_Stoat_" + id;
+            incrementCounter(); //increment the counter
+            track.name = baseName + imageTarget_counts[baseName];
         }
 
-        // if the state was tracked, but it isn't anymore ...
-        if (status.Status != Status.TRACKED && previousState == Status.TRACKED){
+        // if the state was tracked/extended tracked, but it isn't anymore ...
+        if (status.Status != Status.TRACKED && status.Status != Status.EXTENDED_TRACKED && previousState == Status.TRACKED)
+        {
             // ... then we should delete this ImageTarge
             Debug.Log(gameObject.name + " | Was tracked before, but now it is not");
             Destroy(gameObject, 0);
@@ -51,6 +51,14 @@ public class StoatTrackingStatusObserver : MonoBehaviour
             mObserverBehaviour.OnTargetStatusChanged -= OnStatusChanged;
     }
 
+    private void incrementCounter()
+    {
+        // if this is not present in our dictionary of imageTargets we are counting; add it (initialising count to 0)
+        if (!imageTarget_counts.ContainsKey(baseName))
+            imageTarget_counts.Add(baseName, 0);
+        // else, increment the counter for this imageTarget
+        else
+            imageTarget_counts[baseName]++;
+    }
+
 }
-
-
